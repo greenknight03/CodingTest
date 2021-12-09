88bb870e0825c49a6e9cb8c72401317c83d37dea
HeeJeongHan <92960723+greenknight03@users.noreply.github.com>
2021-12-10 01:15:30 +0900

    JS020

--- JS100/020.js
+++ JS100/020.js
@@ -1,8 +1,6 @@
 // 공백으로 구분하여 두 숫자가 주어집니다.
 // 두번째 숫자로 첫번째 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력
 
-const num = ["7", "2"];
-
 const n = prompt("수를 입력하세요.").split(" ");
 
 const result = Math.floor(parseInt(n[0], 10) / parseInt(n[1], 10));
