2a11ed267998aeaf53a49152f74fd4484741f472
HeeJeongHan <92960723+greenknight03@users.noreply.github.com>
2021-12-10 01:10:55 +0900

    JS019-020

---
+++ JS100/020.js
@@ -0,0 +1,13 @@
+// 공백으로 구분하여 두 숫자가 주어집니다.
+// 두번째 숫자로 첫번째 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력
+
+const num = ["7", "2"];
+
+const n = prompt("수를 입력하세요.").split(" ");
+
+const result = Math.floor(parseInt(n[0], 10) / parseInt(n[1], 10));
+const left = parseInt(n[0], 10) % parseInt(n[1], 10);
+
+console.log(result, left);
+
+//  Math.floor 함수는 보다 작은 최대의 정수를 반환
